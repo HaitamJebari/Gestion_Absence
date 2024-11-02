116b738c7f5ab41a333aa46aa62066ad03dd1c29
Haitam Jebari <hjebari@digital-place.co>
2024-11-02 11:30:26 +0100

    First commit

---
+++ app/src/main/java/com/ahmedrem/projet_android_dam/MainActivity.java
@@ -0,0 +1,53 @@
+package com.ahmedrem.projet_android_dam;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText username , password;
+    MaterialButton login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usr,psw;
+                usr = username.getText().toString();
+                psw = password.getText().toString();
+
+                if(username.equals("Mohamed") && psw.equals("123")){
+                    Snackbar.make(view,"Login Successfully",0).show();
+
+                }
+                else {
+                    Snackbar.make(view,"Error in Login ", 0).show();
+                }
+            }
+        });
+    }
+}
